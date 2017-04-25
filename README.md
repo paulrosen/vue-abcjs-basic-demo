31436cc9d6ed4dc051bcd76c9ee79fb4d7088e65
Paul Rosen <paul@paulrosen.net>
2017-04-24 21:15:44 -0400

    Simple Vue application that displays sheet music using abcjs to render ABC Notation.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # vue-abcjs-basic-demo
-Simplest demo for using ABC notation in a website.
+> Simplest abcjs demo for npm
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/Hello.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="hello">
+		<h1>Vue Basic abcjs Demo</h1>
+		<p>This is a simple app that just displays some sheet music.</p>
+		<p>The only addition to the standard vue-cli app's dependencies is:</p>
+		<pre>npm install abcjs --save</pre>
+		<p>The only necessary code to make this work is:</p>
+		<pre>import abcjs from "abcjs";
+abcjs.renderAbc("paper", this.tune);</pre>
+		<textarea id="abc-source"></textarea>
+		<div id="midi"></div>
+		<div id="paper"></div>
+	</div>
+</template>
+
+<script>
+	import abcjs from "abcjs";
+
+	export default {
+		mounted: function () {
+			abcjs.renderAbc("paper", this.tune);
+		},
+		name: 'hello',
+		data () {
+			return {
+				tune: 'X:1\nT: Cooley\'s\nM: 4/4\nL: 1/8\nR: reel\nK: Emin\nD2|:"Em"EB{c}BA B2 EB|~B2 AB dBAG|"D"FDAD BDAD|FDAD dAFD|\n"Em"EBBA B2 EB|B2 AB defg|"D"afe^c dBAF|1"Em"DEFD E2 D2:|2"Em"DEFD E2 gf||\n|:"Em"eB B2 efge|eB B2 gedB|"D"A2 FA DAFA|A2 FA defg|\n"Em"eB B2 eBgB|eB B2 defg|"D"afe^c dBAF|1"Em"DEFD E2 gf:|2"Em"DEFD E4|]\n'
+			}
+		}
+	}
+</script>
+
+<style>
+	#abc-source {
+		width: 350px;
+		height: 300px;
+	}
+
+	pre {
+		border: 1px solid #888888;
+		padding: 6px;
+		border-radius: 4px;
+		width: 350px;
+	}
+
+</style>
